e8b7addbdf1d1c964bfa8dfb3001c0293e8e0514
Naseem Makiya <Naseem.makiya@moovweb.com>
2016-01-21 09:49:32 -0500

    Update sunburst.js

--- inst/htmlwidgets/sunburst.js
+++ inst/htmlwidgets/sunburst.js
@@ -363,32 +363,29 @@ HTMLWidgets.widget({
           var nodeName = parts[j];
           var childNode;
           if (j + 1 < parts.length) {
-       // Not yet at the end of the sequence; move down the tree.
-     	var foundChild = false;
-     	for (var k = 0; k < children.length; k++) {
-     	  if (children[k]["name"] == nodeName) {
-     	    childNode = children[k];
-     	    foundChild = true;
-     	    break;
-     	  }
-     	}
-      // If we don't already have a child node for this branch, create it.
-     	if (!foundChild) {
-     	  childNode = {"name": nodeName, "children": []};
-     	  children.push(childNode);
-     	}
-     	currentNode = childNode;
+         // Not yet at the end of the sequence; move down the tree.
+         	var foundChild = false;
+         	for (var k = 0; k < children.length; k++) {
+         	  if (children[k]["name"] == nodeName) {
+         	    childNode = children[k];
+         	    foundChild = true;
+         	    break;
+         	  }
+         	}
+          // If we don't already have a child node for this branch, create it.
+         	if (!foundChild) {
+         	  childNode = {"name": nodeName, "children": []};
+         	  children.push(childNode);
+         	  currentNode = childNode;
           } else {
-     	// Reached the end of the sequence; create a leaf node.
-     	childNode = {"name": nodeName, "size": size};
-     	children.push(childNode);
+             	// Reached the end of the sequence; create a leaf node.
+             	childNode = {"name": nodeName, "size": size};
+             	children.push(childNode);
           }
         }
       }
-      return root;
     };
-
-
+    return root;
   },
 
   resize: function(el, width, height, instance) {
